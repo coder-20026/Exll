685e2c84c4bd2957a82387871ca30cc8ad438ddc
Coder-1891 <cchauhan.alpesh24598@gmail.com>
2026-04-02 16:42:47 +0530

    Replace calculateSummaryTotals with calculateTotalAmount

--- ExcelTableApp/app/src/main/java/com/whtstable/master/MainActivity.java
+++ ExcelTableApp/app/src/main/java/com/whtstable/master/MainActivity.java
@@ -342,7 +342,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 cell.setText(input.getText().toString());
-                calculateSummaryTotals();
+                calculateTotalAmount();
             }
         });
         
